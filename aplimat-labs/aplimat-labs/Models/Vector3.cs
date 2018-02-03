22ca1094be5a427d30fc775e0fee48ca81b506aa
Ralph Kevin Tan <tan_ralphkevin@yahoo.com>
2018-02-03 14:20:26 +0800

    Quiz#1: Starry Night 1 xPos = good 2 yPos = good 3 colors = good bonus 1 size = nope bonus 2 clear = good

---
+++ aplimat-labs/aplimat-labs/Models/Vector3.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aplimat_labs.Models
+{
+    public class Vector3
+    {
+        public float x, y, z;
+
+        public Vector3()
+        {
+            this.x = 0;
+            this.y = 0;
+            this.z = 0;
+        }
+
+        public Vector3(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public Vector3(double _x, double _y, double _z)
+        {
+            x = (float)_x;
+            y = (float)_y;
+            z = (float)_z;
+        }
+
+
+        public static Vector3 operator + (Vector3 left, Vector3 right)
+        {
+            return new Vector3(left.x + right.x,
+                               left.y + right.y,
+                               left.z + right.z);
+        }
+
+        public static Vector3 operator - (Vector3 left, Vector3 right)
+        {
+            return new Vector3(left.x - right.x,
+                               left.y - right.y,
+                               left.z - right.z);
+        }
+    }
+}
